4c550efa5a37be9ef3b461a6894e7f08b864490a
Dionnatan Alves <dionn_a.p@hotmail.com>
2023-02-06 02:22:17 -0300

    doc: importing components and styling configuration from App.js file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import './App.css';
+import { ContainerForm } from './components/ContainerForm';
+import { ContainerLeft } from './components/ContainerLeft';
+import { Footer } from './components/Footer';
+import { SideBar } from './components/SideBar';
+import './styles/global.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className='flex flex-col w-screen h-screen justify-between items-center font-poppins'>
+      <div className='w-full h-full flex flex-row items-center justify-between px-5'>
+        <div className="flex w-2/4 h-full flex-col items-center justify-center">
+          <ContainerLeft />
+        </div>
+        <div className="flex w-2/4 h-full relative flex-col items-start justify-center pl-4">
+          <SideBar />
+          <div className="flex flex-col">
+            <ContainerForm />
+          </div>
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 }
