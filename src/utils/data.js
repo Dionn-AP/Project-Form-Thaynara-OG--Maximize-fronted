cbed82c10566d3d44a721f5956c0faf9074d0161
Dionnatan Alves <dionn_a.p@hotmail.com>
2023-02-06 02:31:11 -0300

    doc: creation of image file for the sidebar

---
+++ src/utils/data.js
@@ -0,0 +1,33 @@
+import IconInsta from '../assets/icon-instagram.svg';
+import IconYoutube from '../assets/icon-youtube.svg';
+import IconTwitter from '../assets/icon-twitter.svg';
+import IconFace from '../assets/icon-facebook.svg';
+import IconSnap from '../assets/icon-snapchat.svg';
+import IconTiktok from '../assets/icon-tiktok.svg';
+
+export const dataImages = [
+    {
+        image: `${IconInsta}`,
+        alt: "Instagram"
+    },
+    {
+        image: `${IconYoutube}`,
+        alt: "Youtube"
+    },
+    {
+        image: `${IconTwitter}`,
+        alt: "Twitter"
+    },
+    {
+        image: `${IconFace}`,
+        alt: "Facebook"
+    },
+    {
+        image: `${IconSnap}`,
+        alt: "Snapchat"
+    },
+    {
+        image: `${IconTiktok}`,
+        alt: "Tiktok"
+    }
+]
