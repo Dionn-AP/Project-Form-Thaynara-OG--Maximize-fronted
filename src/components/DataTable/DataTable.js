155f63ae9a1ddad219c8d1267b2eb876c4795c84
Dionnatan Alves <dionn_a.p@hotmail.com>
2023-02-09 15:28:05 -0300

    doc: add loading in data table

--- src/components/DataTable/DataTable.js
+++ src/components/DataTable/DataTable.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { SpinningCircles } from 'react-loading-icons';
 import { getItem, getHeaders } from '../../utils/functions';
 import api from '../../services/api';
 import clsx from 'clsx';
@@ -7,16 +8,20 @@ import { Message } from '../Message';
 export function DataTable({ openMessage, setOpenMessage }) {
     const token = getItem('token');
     const [allMessages, setAllMessages] = useState([]);
-    const [currentMessage, setCurrentMessage] = useState([])
+    const [currentMessage, setCurrentMessage] = useState([]);
+    const [loading, setLoading] = useState(false);
 
     async function listAllMessages() {
+        setLoading(true);
         try {
             await api.get('/viewmessages', getHeaders(token))
                 .then(response => {
                     setAllMessages(response.data)
                 })
+            setLoading(false);
 
         } catch (error) {
+            setLoading(false)
             return
         }
     }
@@ -56,29 +61,37 @@ export function DataTable({ openMessage, setOpenMessage }) {
             </div>
             <div className='grid grid-rows-7 grid-flow-row gap-3 w-full py-5 px-4 bg-background shadow-md'>
                 {
-                    !allMessages.length ?
-                        <span className='text-base font-semibold text-center text-text_color'>
-                            Não há mensagens na sua caixa de entrada no momento
-                        </span>
+                    loading ?
+                        <div className='flex mt-6 item-center justify-center h-auto w-full'>
+                            <SpinningCircles
+                                speed={1.2}
+                                fill='#FF8585'
+                            />
+                        </div>
                         :
-                        allMessages.map((message) => {
-                            return (
-                                <div
-                                    key={message.id}
-                                    onClick={() => viewMessage(message)}
-                                    className='flex flex-col items-start justify-center w-full truncate border-b cursor-pointer border-border_inputs hover:bg-zinc-100 transition-colors'
-                                >
-                                    <span className='text-sm font-bold'>{message.sender_name}</span>
-                                    <span className='text-xs font-thin'>{message.contact_reference}</span>
-                                    <p
-                                        className={clsx('text-sm font-normal text-color_black_default truncate py-2', {
-                                            "font-semibold": !message.message_read
-                                        })}>
-                                        {message.sender_message}
-                                    </p>
-                                </div>
-                            )
-                        })}
+                        !allMessages.length ?
+                            <span className='text-base font-semibold text-center text-text_color'>
+                                Não há mensagens na sua caixa de entrada no momento
+                            </span>
+                            :
+                            allMessages.map((message) => {
+                                return (
+                                    <div
+                                        key={message.id}
+                                        onClick={() => viewMessage(message)}
+                                        className='flex flex-col items-start justify-center w-full truncate border-b cursor-pointer border-border_inputs hover:bg-zinc-100 transition-colors'
+                                    >
+                                        <span className='text-sm font-bold'>{message.sender_name}</span>
+                                        <span className='text-xs font-thin'>{message.contact_reference}</span>
+                                        <p
+                                            className={clsx('text-sm font-normal text-color_black_default truncate py-2', {
+                                                "font-semibold": !message.message_read
+                                            })}>
+                                            {message.sender_message}
+                                        </p>
+                                    </div>
+                                )
+                            })}
             </div>
         </div>
     );
